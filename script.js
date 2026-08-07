ef98f9f51649bc2c0bfd214e9da2ebafe54ba9b6
asiyamilan <asiyamilan2001@gmail.com>
2026-08-07 11:59:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-const themeToggle = document.getElementById("themeToggle");
+document.addEventListener("DOMContentLoaded", function () {
 
+    const themeToggle = document.getElementById("themeToggle");
 
-// Load saved theme
-const savedTheme = localStorage.getItem("financeTheme");
+    // Check saved theme
+    const savedTheme = localStorage.getItem("financeTheme");
 
-if (savedTheme === "dark") {
-    document.body.classList.add("dark-mode");
-    themeToggle.textContent = "☀️";
-}
-
-
-
-// Theme switch button
-
-themeToggle.addEventListener("click", () => {
-
-    document.body.classList.toggle("dark-mode");
-
-
-    if (document.body.classList.contains("dark-mode")) {
-
-        localStorage.setItem("financeTheme", "dark");
-        themeToggle.textContent = "☀️";
-
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark-mode");
+        themeToggle.innerHTML = "☀️";
     } else {
-
-        localStorage.setItem("financeTheme", "light");
-        themeToggle.textContent = "🌙";
-
+        themeToggle.innerHTML = "🌙";
     }
+
+
+    // Button click
+    themeToggle.addEventListener("click", function () {
+
+        document.body.classList.toggle("dark-mode");
+
+
+        if (document.body.classList.contains("dark-mode")) {
+
+            localStorage.setItem("financeTheme", "dark");
+            themeToggle.innerHTML = "☀️";
+
+        } else {
+
+            localStorage.setItem("financeTheme", "light");
+            themeToggle.innerHTML = "🌙";
+
+        }
+
+    });
 
 });
